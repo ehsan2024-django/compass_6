128de0909eb6945d767940cb60b33553a306ad73
ehsan2024-django <mortezamojtaba269@gmail.com>
2025-01-29 19:07:42 +0330

    Ready for production1

--- compass_1_django/settings.py
+++ compass_1_django/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+(0h!n%ln=2cgmu8-=ce%st6r*gf_ns*%okamgc#hu24n3v+=8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','testkeshmun.ir', 'www.testkeshmun.ir', '194.5.207.143']
 
@@ -118,35 +118,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATIC_URL = 'static/'
-
-
 STATIC_URL = '/static/'
-
-#STATIC_URL = '/static/'
-#STATICFILES_DIRS = [
-    #BASE_DIR / "static",
-#]
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = '/app/media'
-#STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
-
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-#STATICFILES_DIRS = [
-    #BASE_DIR / "static",  # این مسیر باید به پوشه static شما اشاره کند
-#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
